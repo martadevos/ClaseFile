d7f7fd459d72bf0c9f5fb11bfc39a7ab5e367256
martadevos <marta.devos@iesnervion.es>
2022-04-25 14:37:09 +0200

    .

--- src/Ejercicio1/E1.java
+++ src/Ejercicio1/E1.java
@@ -5,4 +5,7 @@ public class E1 {
     public static final String RUTA = "src/Ficheros";
     public static final String FICHERO = "";
 
+    public static void consulta (){
+        System.out.println("Introduzca el fichero que desea consultar");
+    }
 }
